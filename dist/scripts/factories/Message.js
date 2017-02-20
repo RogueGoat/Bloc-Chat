108c394f47d59eb7747d2a0cb6d34dd6d76dbc76
Florence <roguegoatstudios@outlook.com>
2017-02-20 13:39:55 -0600

    working on layout

---
+++ dist/scripts/factories/Message.js
@@ -0,0 +1,14 @@
+(function() {
+     var ref = firebase.database().ref().child("messages");
+     var messages = $firebaseArray(ref);
+
+     return {
+         getByRoomId: function (roomId) {
+         // Filter the messages by their room ID.
+      }
+    };
+  }
+  angular
+    .module('blocChat')
+    .factory('Message', ['$firebaseArray', Message]);
+})();

6bfedced93565071764d490294afabffed7dddae
Florence <roguegoatstudios@outlook.com>
2017-02-20 23:11:03 -0600

    adding message functionality

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,14 @@
 (function() {
-    function HomeCtrl($scope, Room, $uibModal) {
+    function HomeCtrl($scope, Room, $uibModal, Message) {
         $scope.roomList = Room.all;
+        $scope.messages = {};
+        
+        // scope to toggle room title
+        
+        $scope.setCurrentRoom = function(room){
+            $scope.currentRoom = room;
+            $scope.messages = Message.getByRoomId(room.$id);
+        };
         
         $scope.addChatRoom = function(){
             $uibModal.open({
@@ -13,5 +21,5 @@
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['$scope', 'Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['$scope', 'Room', '$uibModal', 'Message', HomeCtrl]);
 })();
--- dist/scripts/factories/Message.js
+++ dist/scripts/factories/Message.js
@@ -1,14 +1,15 @@
-//(function() {
-//     var ref = firebase.database().ref().child("messages");
-//     var messages = $firebaseArray(ref);
-//
-//     return {
-//         getByRoomId: function (roomId) {
-//         // Filter the messages by their room ID.
-//      }
-//    };
-//  }
-//  angular
-//    .module('blocChat')
-//    .factory('Message', ['$firebaseArray', Message]);
-//})();
+(function() {
+    function Message($firebaseArray) {
+     var ref = firebase.database().ref().child("messages");
+     var messages = $firebaseArray(ref);
+
+     return {
+         getByRoomId: function (roomId) {
+         $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
+      }
+    };
+  }
+  angular
+    .module('blocChat')
+    .factory('Message', ['$firebaseArray', Message]);
+})();

3ce33abee374825f1000349f56dcaf7f0e418be7
Florence <roguegoatstudios@outlook.com>
2017-02-25 17:53:15 -0600

    added user bar at bottom

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,11 +1,12 @@
 (function() {
+    // this file and function control the means necessary to show the app at all. We connect main templates and controllers here and initialize them
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
                 enabled: false,
                 requireBase: false
             });
-        
+        // makes the homectrl and home template what appears when all this information is passed into the index html page
         $stateProvider
             .state('home', {
                 url: '',
@@ -14,7 +15,7 @@
             });
         
     }
-    
+    // configures entire app
     angular
         .module('blocChat', ['ui.router', 'firebase', 'ui.bootstrap', 'ngCookies'])
         .config(config);
--- dist/scripts/factories/Message.js
+++ dist/scripts/factories/Message.js
@@ -1,14 +1,19 @@
 (function() {
+    //     This $firebaseArray service takes a Firebase reference or Firebase Query and returns a JavaScript object which contains the data at the provided Firebase reference
+    // $cookies service acts to save data at a given place in the application
     function Message($firebaseArray, $cookies) {
+     //This variable holds the information in the firebase database that oversees messages posted
      var ref = firebase.database().ref().child("messages");
+     // This variable holds the $firebaseArray database service and references what's in it
      var messages = $firebaseArray(ref);
-//this should return messages
+     
+        //this should return messages
      return {
          getByRoomId: function (roomId) {
          $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
       },
          send: function(newMessage, currentRoomId) {
-         // Send method logic
+         // This logic should return the information needed for the message
              messages.$add ({
                username: $cookies.get('blocChatCurrentUser'),
                roomId: currentRoomId,
@@ -17,6 +22,7 @@
       }
     };
   }
+    //this file is a factory
   angular
     .module('blocChat')
     .factory('Message', ['$firebaseArray', '$cookies', Message]);
